d4ce5f4960d3e61d0cf616eba062b23c13392c30
ChippedChap <mikiel.theg@gmail.com>
2018-11-23 09:13:23 -0500

    Fix filth outside of area being in the area

--- CleaningPriority/ListerFilthInAreas_MapComponent.cs
+++ CleaningPriority/ListerFilthInAreas_MapComponent.cs
@@ -69,6 +69,7 @@ namespace CleaningPriority
 
 		public void OnAreaChange(IntVec3 cell, bool newVal, Area area)
 		{
+			if (!area[cell]) return;
 			List<Thing> thingsInCell = cell.GetThingList(map);
 			if (newVal)
 			{
